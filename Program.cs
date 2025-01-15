461f046674c1b6e00af1bfd29f5dbb85b9fc838a
saldy <116149257+ya-lybly-alisy@users.noreply.github.com>
2025-01-15 14:37:27 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace saldy_consapp
+{
+
+}
+
+
+class Computer
+{
+    // Открытые поля
+    public int InventoryNumber { get; set; }
+    public double ProcessorFrequency { get; set; }
+    public int HardDiskCapacity { get; set; }
+
+    // Метод для отображения данных о компьютере
+    public void ShowInfo()
+    {
+        Console.WriteLine($"Инвентарный номер: {InventoryNumber}");
+        Console.WriteLine($"Частота процессора: {ProcessorFrequency} ГГц");
+        Console.WriteLine($"Объем жесткого диска: {HardDiskCapacity} ГБ");
+    }
+
+    // Метод расчета стоимости внешней памяти
+    public double CalculateStorageCost(double costPerGigabyte)
+    {
+        return HardDiskCapacity * costPerGigabyte;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Создание объекта Computer
+        Computer myComputer = new Computer
+        {
+            InventoryNumber = 123345,
+            ProcessorFrequency = 1.5,
+            HardDiskCapacity = 400
+        };
+
+        // Вывод значений свойств объекта
+        myComputer.ShowInfo();
+
+        // Расчет стоимости внешней памяти
+        double costPerGigabyte = 100.0;
+        double totalCost = myComputer.CalculateStorageCost(costPerGigabyte);
+
+        // Вывод стоимости на печать
+        Console.WriteLine($"Стоимость внешней памяти: {totalCost} рублей");
+        Console.WriteLine("Выполнил Сладков Даниил 34ИС");
+            Console.ReadKey(); 
+    }
+}
+
+
+
